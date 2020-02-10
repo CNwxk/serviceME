6ee0a26b872fdda69a09346c81cd0a327af6d814
王霄琨 <wangxiaokun@myhost.local>
2020-02-09 19:49:59 -0600

    Create Environment

---
+++ src/main/java/com/team6/serviceme/ServicemeApplication.java
@@ -0,0 +1,13 @@
+package com.team6.serviceme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicemeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team6/serviceme/ServicemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team6.serviceme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
